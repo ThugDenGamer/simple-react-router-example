7acb21ca404a532efd60c851308c405a79e693c3
ThugDenGamer <thugdengamer@gmail.com>
2020-10-05 19:03:23 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import { BrowserRouter as Router,Route,Redirect,Link,Switch } from "react-router-dom";
 import './App.css';
+import Home from './Components/Home';
+import Profile from './Components/Profile';
+import News from './Components/News';
+import Error from './Components/Error';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loggedIn: false
+  }
+  changeState = () => {
+    this.setState({
+      loggedIn: !this.state.loggedIn
+    })
+  }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Link exact to="/">Home</Link><br/>
+          <Link exact to="/news">News</Link><br/>
+          <Link exact to="/profile">Profile</Link>
+          <br/><br/>
+          <input
+            type="button"
+            onClick={this.changeState}
+            value={this.state.loggedIn ? "Logout" : "Login"}
+          />
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/news" exact component={News}/>
+            <Route path="/profile" exact render={() => this.state.loggedIn ? (<Profile/>) : (<Redirect to="/"/>)}/>
+            <Route component={Error}/>          
+          </Switch>
+        </div>  
+      </Router>
+    );
+  }
 }
 
 export default App;
